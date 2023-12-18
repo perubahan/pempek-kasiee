36a81ebb66dda8f0427d167493508b33378a41e6
perubahan <138684874+perubahan@users.noreply.github.com>
2023-12-18 09:50:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60) {
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+      document.querySelector('#scroll-top').classList.remove('active'); 
+  }
+}
+
+var swiper = new Swiper(".home", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
+});
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
